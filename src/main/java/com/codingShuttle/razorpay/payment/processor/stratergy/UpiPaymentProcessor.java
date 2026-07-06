38d2e55c78fee4385dba188fb75efe1b5a35d1af
Octar123 <ranjanassmali123@gmail.com>
2026-07-06 20:57:27 +0530

    Implemented the charge functionality;

--- src/main/java/com/codingShuttle/razorpay/payment/processor/stratergy/UpiPaymentProcessor.java
+++ src/main/java/com/codingShuttle/razorpay/payment/processor/stratergy/UpiPaymentProcessor.java
@@ -1,5 +1,6 @@
 package com.codingShuttle.razorpay.payment.processor.stratergy;
 
+import com.codingShuttle.razorpay.common.util.RandomizerUtil;
 import com.codingShuttle.razorpay.payment.processor.PaymentProcessor;
 import com.codingShuttle.razorpay.payment.processor.dto.PaymentProcessorRequest;
 import com.codingShuttle.razorpay.payment.processor.dto.PaymentProcessorResponse;
@@ -7,6 +8,22 @@ import com.codingShuttle.razorpay.payment.processor.dto.PaymentProcessorResponse
 public class UpiPaymentProcessor implements PaymentProcessor {
     @Override
     public PaymentProcessorResponse charge(PaymentProcessorRequest request) {
-        return null;
+
+        final String VPA_CODE_FAIL = "fail@okaxis";
+
+        String bankCode = request.methodDetails() != null
+                ? request.methodDetails().get("vpa").toString()
+                : null;
+
+        if(VPA_CODE_FAIL.equals(bankCode)){
+            return new PaymentProcessorResponse.Failure("BANK_REJECTED",
+                    "Bank rejected the transaction registration.");
+        }
+
+        String processorRef = "UPI_PROCESSOR_" + RandomizerUtil.randomBase64(16);
+
+        String bankRef = "BANK_REF" + RandomizerUtil.randomBase64(16);
+
+        return new PaymentProcessorResponse.Success(processorRef, bankRef);
     }
 }
