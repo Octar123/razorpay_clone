990780d6634a60410521e4778405476d2c935133
Octar123 <ranjanassmali123@gmail.com>
2026-07-06 20:52:45 +0530

    Created the invalid state transition exception class for state machine; Created the Payment State machine; Created the payment transition log repository; Created the payment transition service to log the transitions.

---
+++ src/main/java/com/codingShuttle/razorpay/payment/statemachine/PaymentTransitionService.java
@@ -0,0 +1,38 @@
+package com.codingShuttle.razorpay.payment.statemachine;
+
+import com.codingShuttle.razorpay.common.enums.PaymentActor;
+import com.codingShuttle.razorpay.common.enums.PaymentEvent;
+import com.codingShuttle.razorpay.common.enums.PaymentStatus;
+import com.codingShuttle.razorpay.payment.entity.Payment;
+import com.codingShuttle.razorpay.payment.entity.PaymentTransitionLog;
+import com.codingShuttle.razorpay.payment.repository.PaymentTransitionLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentTransitionService {
+
+    private final PaymentTransitionLogRepository paymentTransitionLogRepository;
+    private final PaymentStateMachine paymentStateMachine;
+
+    public PaymentStatus apply(Payment payment, PaymentEvent event) {
+        PaymentStatus next = paymentStateMachine.transition(payment.getStatus(), event);
+        payment.setStatus(next);
+
+        PaymentTransitionLog log = PaymentTransitionLog.builder()
+                .payment(payment)
+                .fromStatus(payment.getStatus())
+                .event(event)
+                .toStatus(next)
+                .actor(PaymentActor.SYSTEM) //TODO:- fetch merchant context to identify actor
+                .occurredAt(LocalDateTime.now())
+                .build();
+
+        paymentTransitionLogRepository.save(log);
+
+        return next;
+    }
+}
