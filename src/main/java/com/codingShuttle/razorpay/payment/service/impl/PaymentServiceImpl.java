c6dc043bd784af86640843d3c766c9bb2cf6778d
Octar123 <ranjanassmali123@gmail.com>
2026-07-15 14:59:07 +0530

    Simulated the bank callback in BankCallbackSimulator; Implemented the resolveAuthorization; Added the required dependancies for jwt in pom.xml

--- src/main/java/com/codingShuttle/razorpay/payment/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/codingShuttle/razorpay/payment/service/impl/PaymentServiceImpl.java
@@ -57,6 +57,7 @@ public class PaymentServiceImpl implements PaymentService {
                 .amount(order.getAmount())
                 .status(PaymentStatus.CREATED)
                 .method(request.method())
+                .idempotencyKey(UUID.randomUUID().toString()) //TODO: idempotency
                 .methodDetails(request.methodDetails())
                 .build();
 
@@ -66,6 +67,8 @@ public class PaymentServiceImpl implements PaymentService {
                 request.orderId(), merchantId,
                 order.getAmount(), request.method(),
                 request.methodDetails());
+
+        paymentTransitionService.apply(payment, PaymentEvent.AUTHORIZE_ATTEMPT);
 
         PaymentResult result = paymentGatewayRouter.initiate(paymentRequest);
 
@@ -120,5 +123,50 @@ public class PaymentServiceImpl implements PaymentService {
 
 
         return paymentMapper.toResponse(payment);
+    }
+
+    @Override
+    @Transactional
+    public void resolveAuthorization(UUID paymentId, boolean approve, String bankRef,
+                                     String errorCode, String errorDescription) {
+        Payment payment = paymentRepository.findById(paymentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Payment", paymentId));
+
+        if(payment.getStatus() != PaymentStatus.AUTHORIZING) {
+            log.warn("Payment is not in Authorizing state, paymentId: {}, status: {}", paymentId, payment.getStatus());
+            return;
+        }
+
+        OrderRecord orderRecord = payment.getOrder();
+
+        if(approve){
+            paymentTransitionService.apply(payment, PaymentEvent.AUTHORIZE_SUCCESS);
+            payment.setBankReference(bankRef);
+            payment.setAuthorizedAt(LocalDateTime.now());
+
+//            Auto-capture
+            paymentTransitionService.apply(payment, PaymentEvent.CAPTURE_REQUEST);
+            PaymentResult captureResult = paymentGatewayRouter.capture(payment.getMethod(), paymentId);
+
+            if (captureResult instanceof PaymentResult.Success success) {
+                paymentTransitionService.apply(payment, PaymentEvent.CAPTURE_SUCCESS);
+                payment.setCapturedAt(LocalDateTime.now());
+                orderRecord.setOrderStatus(OrderStatus.PAID);
+            }else if(captureResult instanceof PaymentResult.Failure failure){
+                paymentTransitionService.apply(payment, PaymentEvent.CAPTURE_FAIL);
+                payment.setErrorCode(failure.errorCode());
+                payment.setErrorDescription(failure.errorDescription());
+            }
+        }else {
+            paymentTransitionService.apply(payment, PaymentEvent.AUTHORIZE_FAIL);
+            payment.setErrorCode(errorCode);
+            payment.setErrorDescription(errorDescription);
+        }
+
+        paymentRepository.save(payment);
+        orderRepository.save(orderRecord);
+
+        // TODO:- send an outbox(kafka event)
+
     }
 }
--- src/main/java/com/codingShuttle/razorpay/payment/simulator/BankCallbackSimulator.java
+++ src/main/java/com/codingShuttle/razorpay/payment/simulator/BankCallbackSimulator.java
@@ -1,6 +1,9 @@
 package com.codingShuttle.razorpay.payment.simulator;
 
+import com.codingShuttle.razorpay.common.enums.ChaosMode;
+import com.codingShuttle.razorpay.common.enums.PaymentMethod;
 import com.codingShuttle.razorpay.common.enums.PaymentStatus;
+import com.codingShuttle.razorpay.common.util.RandomizerUtil;
 import com.codingShuttle.razorpay.payment.entity.Payment;
 import com.codingShuttle.razorpay.payment.repository.PaymentRepository;
 import com.codingShuttle.razorpay.payment.service.PaymentService;
@@ -21,13 +24,15 @@ public class BankCallbackSimulator {
     private final PaymentService paymentService;
     private final SimulatorConfig simulatorConfig;
 
-    @Scheduled(fixedDelayString = "{payment.simulator.poll-interval-ms:5000}")
+//    @Scheduled(fixedDelayString = "${payment.simulator.poll-interval-ms:5000}")
     public void processCallbacks(){
 
         LocalDateTime globalWindow = LocalDateTime.now().minusSeconds(1);
 
         List<Payment> candidates = paymentRepository
                 .findByStatusAndCreatedAtBefore(PaymentStatus.AUTHORIZING, globalWindow);
+
+        log.info("Simulating payments for {} payments", candidates.size());
 
         if (candidates.isEmpty()) return;
 
@@ -37,5 +42,53 @@ public class BankCallbackSimulator {
     }
 
     private void simulateCallback(Payment payment) {
+
+        SimulatorConfig.MethodSimulatorConfig methodConfig = simulatorConfig.configFor(payment.getMethod());
+
+        LocalDateTime dueAt = dueAt(payment, methodConfig);
+
+        if(LocalDateTime.now().isBefore(dueAt)) {
+            return;
+        }
+
+        ChaosMode chaosMode = simulatorConfig.getChaosMode();
+
+        switch (chaosMode) {
+            case SUCCESS -> resolve(payment, true);
+            case FAILURE -> resolve(payment, false);
+            case TIMEOUT -> {
+                log.debug("BankCallback simulator: Payment timed out");
+            }
+            case NORMAL, SLOW -> resolve(payment, shouldApprove(payment, methodConfig));
+        }
+    }
+
+    private void resolve(Payment payment, boolean approve){
+        if(approve){
+            String bankRef = "SIM_BANK_REF"+ RandomizerUtil.randomBase64(8);
+            paymentService.resolveAuthorization(payment.getId(), true, bankRef, null, null);
+        }else{
+            paymentService.resolveAuthorization(payment.getId(), false, null, "SIM_BANK_ERROR_CODE", "Simulated Bank Decline");
+
+        }
+    }
+
+    private boolean shouldApprove(Payment payment, SimulatorConfig.MethodSimulatorConfig methodConfig) {
+        int bucket = Math.abs(payment.getId().hashCode()) % 100;
+
+        return bucket < methodConfig.getSuccessRate();
+    }
+
+    private LocalDateTime dueAt(Payment payment, SimulatorConfig.MethodSimulatorConfig methodConfig) {
+
+        int range = methodConfig.getMaxDelaySeconds() - methodConfig.getMinDelaySeconds();
+
+        int delaySeconds = methodConfig.getMinDelaySeconds() + Math.abs(payment.getId().hashCode()) % (range+1);
+
+        if(simulatorConfig.getChaosMode() == ChaosMode.SLOW) {
+            delaySeconds *= 2;
+        }
+
+        return payment.getCreatedAt().plusSeconds(delaySeconds);
     }
 }
