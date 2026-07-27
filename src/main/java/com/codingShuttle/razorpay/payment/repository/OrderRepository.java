d7e27316032fe8f3c7ff0bbfddffce5ec36ecb2a
Octar123 <ranjanassmali123@gmail.com>
2026-07-27 20:03:15 +0530

    Added the Pessimistic locked query in OrderRepository and PaymentRepository; Updated the repository calls to the For Update in PaymentServiceImpl; Added Enumerated Annotation for brand in VaultCard; Added Transactional annotation over the charge method in VaultServiceImpl;

--- src/main/java/com/codingShuttle/razorpay/payment/repository/OrderRepository.java
+++ src/main/java/com/codingShuttle/razorpay/payment/repository/OrderRepository.java
@@ -2,7 +2,11 @@ package com.codingShuttle.razorpay.payment.repository;
 
 import com.codingShuttle.razorpay.payment.dto.response.OrderResponse;
 import com.codingShuttle.razorpay.payment.entity.OrderRecord;
+import jakarta.persistence.LockModeType;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,4 +16,8 @@ public interface OrderRepository extends JpaRepository<OrderRecord, UUID> {
     boolean existsByMerchantIdAndReceipt(UUID merchantId, String receipt);
 
     Optional<OrderRecord> findByIdAndMerchantId(UUID orderId, UUID merchantId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select o from OrderRecord o where o.id = :uuid and o.merchantId = :merchantId")
+    Optional<OrderRecord> findByIdAndMerchantIdForUpdate(UUID uuid, UUID merchantId);
 }
