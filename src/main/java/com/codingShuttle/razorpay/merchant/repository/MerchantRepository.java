b55bb39057bbefded7146fecf0a5acb74d0683b2
Octar123 <ranjanassmali123@gmail.com>
2026-08-19 13:20:25 +0530

    added the new methods in PaymentRepository and MerchantRepository; renamed the MerchantWebhookApi to MerchantLookupService and added new methods; added the thread under the spring in application.yaml;

--- src/main/java/com/codingShuttle/razorpay/merchant/repository/MerchantRepository.java
+++ src/main/java/com/codingShuttle/razorpay/merchant/repository/MerchantRepository.java
@@ -1,10 +1,15 @@
 package com.codingShuttle.razorpay.merchant.repository;
 
+import com.codingShuttle.razorpay.common.enums.MerchantStatus;
 import com.codingShuttle.razorpay.merchant.entity.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
     boolean existsByEmail(String email);
+
+    List<Merchant> findByStatus(MerchantStatus merchantStatus);
 }
